af637bfb6721f8203eb16276e11e8507ebbe33a8
Dhushyanth <dhushyanth5602@gmail.com>
2020-07-01 00:20:13 +0530

    Made basic melody maker

--- laptop/main.py
+++ laptop/main.py
@@ -2,26 +2,30 @@ from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
 from melody import melody
+from kivy.core.window import Window
 
-# Create both screens. Please note the root.manager.current: this is how
-# you can control the ScreenManager from kv. Each screen has by default a
-# property manager that gives you the instance of the ScreenManager used.
+
+obj = melody()
+
+
+Window.fullscreen = 'auto'
+
 
 Builder.load_file("main.kv")
 
 
-# Declare both screens
 class MenuScreen(Screen):
     pass
 
 
 class MelodyMaker(Screen):
     def store(self, id):
-        obj = melody()
         obj.store(id)
 
+    def play(self):
+        obj.play()
 
-# Create the screen manager
+
 sm = ScreenManager()
 sm.add_widget(MenuScreen(name='menu'))
 sm.add_widget(MelodyMaker(name='Melody Maker'))
--- laptop/melody.py
+++ laptop/melody.py
@@ -1,4 +1,5 @@
 from kivy.core.audio.audio_pygame import mixer
+import time
 
 
 class melody():
@@ -6,11 +7,15 @@ class melody():
         super().__init__()
         self.player = []
 
-
     def store(self, id):
-        # self.player = self.player + id
+        self.player.append(id)
         mixer.init()
-        mixer.music.load("sounds/1.mp3")
+        mixer.music.load("D:/Github/Model-laptop/laptop/sounds/%d.mp3" % id)
         mixer.music.play()
 
-
+    def play(self):
+        for i in range(len(self.player)):
+            mixer.init()
+            mixer.music.load("D:/Github/Model-laptop/laptop/sounds/%d.mp3" % self.player[i])
+            mixer.music.play()
+            time.sleep(0.5)
